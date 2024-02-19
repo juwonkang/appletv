826add0c41bf549767bc3f2b3a1c746b07192a14
juwonkang <wndnjs1100@gmail.com>
2024-02-19 23:31:43 +0900

    firstcommit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Nav = () => {
+  return <NavWrapper>Nav</NavWrapper>
+}
+
+export default Nav
---
+++ src/components/필기.js
@@ -0,0 +1,8 @@
+// //원래는
+// fetch("http:www.naver.com)
+// //axios는
+// axios.get("http:www.naver.com)
+
+// axios 인스턴스 만드는 순서
+
+// 중복된 부분을 미리 파일 생성
